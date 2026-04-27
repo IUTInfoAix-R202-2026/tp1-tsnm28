fd06eced8806fc285b491b7c4e667b68f2cd3fb0
Tasnim <tasnimsebri28etude@gmail.com>
2026-04-27 13:52:57 +0000

    Exercice 2 terminé

--- src/main/java/fr/univ_amu/iut/exercice2/StagePersonnalise.java
+++ src/main/java/fr/univ_amu/iut/exercice2/StagePersonnalise.java
@@ -2,6 +2,7 @@ package fr.univ_amu.iut.exercice2;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Exercice 2 - Stage personnalisé.
@@ -18,11 +19,16 @@ public class StagePersonnalise extends Application {
     // TODO exercice 2 : personnaliser le Stage.
     //
     // Les tests attendent, dans l'ordre conseillé :
-    //   1. setTitle("Ma fenêtre personnalisée")
-    //   2. setWidth(500) et setHeight(300)
-    //   3. setResizable(false)
-    //   4. initStyle(StageStyle.UNDECORATED)   ← à appeler avant show()
-    //
+    // 1. setTitle("Ma fenêtre personnalisée")
+    // 2. setWidth(500) et setHeight(300)
+    // 3. setResizable(false)
+    // 4. initStyle(StageStyle.UNDECORATED) ← à appeler avant show()
+    primaryStage.setTitle("Ma fenêtre personnalisée");
+    primaryStage.setWidth(500);
+    primaryStage.setHeight(300);
+    primaryStage.setResizable(false);
+    primaryStage.initStyle(StageStyle.UNDECORATED);
+    primaryStage.show();
     // Active les tests un à un (retire @Disabled) et implémente au fur et
     // à mesure. N'oublie pas d'appeler show() à la fin.
   }
